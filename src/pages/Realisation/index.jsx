f6c5d94141e75110ea4281287ca8ead5dd0c765f
RaphaelB <raphael.bonacina@hotmail.fr>
2024-12-10 10:36:49 +0100

    responsive

--- src/pages/Realisation/index.jsx
+++ src/pages/Realisation/index.jsx
@@ -1,40 +1,46 @@
 import React from 'react';
 import Data from '../../data/index.json'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faReact, faNode } from '@fortawesome/free-brands-svg-icons'
 import './style.scss'
 
 const Realisation = () => {
 
     const ProjectCard = Data.map((Project)=>{
     const encodedTitle = encodeURIComponent(Project.id)
-    const imagePath = require(`../../data/image/${Project.pictures[0]}`) 
      
+    const getIcon = (iconName) => {
+        switch (iconName) {
+          case 'faNode':
+            return faNode;
+          case 'faReact':
+            return faReact;
+        default:
+            return null;
+        }
+      };
 
         return(
             
-            <li key={Project.id} className='cards__list__card'>
-                <div className='cards__list__cardVisuel'>
-                <Link  to={`/realisation/${encodedTitle}`}>
-                <img src={imagePath} alt={Project.title}/>
-                <div className='cards__list__cardText'>
+            <li key={Project.id} >
+                <Link className='cards__list__card' to={`/realisation/${encodedTitle}`}>
+                <FontAwesomeIcon className='cards__list__icons' icon={getIcon(Project.icon)}
+                />
                 <h3>{Project.title}</h3>
                 <p>{Project.shortdescription || "Pas de description disponible"}</p>
-                </div>
                 </Link>
-                </div>
             </li>
                 
         )
     })
     
     return(
-        <div className='cards'>
-        <h1 className='cards__title'> Mes réalisations </h1>   
-        <div>        
+        <div className='realisation'>
+        <h1 className='realisation__title'> Mes réalisations </h1>        
         <ul className='cards__list'>
         {ProjectCard}
-        </ul>  
-        </div>     
+        </ul>    
         </div>
     )
 }
